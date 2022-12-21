3ff70a57bd8bcaeab9bb81c7857179892e300651
skillkrio <skillkrio@gmail.com>
2022-12-21 13:30:10 +0530

    feat:add displaying article using webview in newsarticle details screen

--- lib/screens/news_screen/newsarticle_details_screen.dart
+++ lib/screens/news_screen/newsarticle_details_screen.dart
@@ -1,9 +1,32 @@
 import "package:flutter/material.dart";
 import 'package:flutter_mvvm/viewmodels/newsarticle_viewmodel.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class NewsArticleDetailsScreen extends StatelessWidget {
   final NewsArticleViewModel article;
-  const NewsArticleDetailsScreen({super.key, required this.article});
+  NewsArticleDetailsScreen({super.key, required this.article});
+
+  late final webViewController = WebViewController()
+    ..setJavaScriptMode(JavaScriptMode.unrestricted)
+    ..setBackgroundColor(const Color(0x00000000))
+    ..setNavigationDelegate(
+      NavigationDelegate(
+        onProgress: (int progress) {
+          // Update loading bar.
+          print(progress);
+        },
+        onPageStarted: (String url) {},
+        onPageFinished: (String url) {},
+        onWebResourceError: (WebResourceError error) {},
+        onNavigationRequest: (NavigationRequest request) {
+          if (request.url.startsWith('https://www.youtube.com/')) {
+            return NavigationDecision.prevent;
+          }
+          return NavigationDecision.navigate;
+        },
+      ),
+    )
+    ..loadRequest(Uri.parse(article.url));
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,8 +37,8 @@ class NewsArticleDetailsScreen extends StatelessWidget {
               : "${article.title.substring(0, 25)}...",
         ),
       ),
-      body: const Align(
-        child: Text("Article will be displayed using webview"),
+      body: WebViewWidget(
+        controller: webViewController,
       ),
     );
   }
